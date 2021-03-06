069e9ff71843cc55118ecc9f5037251b2eef5410
Constantin Werner <52051213+constantin50@users.noreply.github.com>
2021-03-06 13:55:32 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -20,13 +20,31 @@ class TextLoader(torch.utils.data.Dataset):
         self.eval = eval
         self.transform = transforms.Compose([
             transforms.ToPILImage(),
-            transforms.Resize((int(hp.height *1.05), int(hp.width *1.05))),
-            transforms.RandomCrop((hp.height, hp.width)),
+            #transforms.Resize((int(hp.height *1.05), int(hp.width *1.05))),
+            #transforms.RandomCrop((hp.height, hp.width)),
+            transforms.ColorJitter(contrast=(0.5,1),saturation=(0.5,1)),
             transforms.RandomRotation(degrees=(-2, 2)),
-            transforms.RandomAffine(10 ,None ,[0.6 ,1] ,3 ,fillcolor=255),
-            #transforms.transforms.GaussianBlur(3, sigma=(0.1, 1.5)),
+            #transforms.RandomAffine(10 ,None ,[0.6 ,1] ,3 ,fillcolor=255),
+            transforms.transforms.GaussianBlur(3, sigma=(0.1, 0.5)),
             transforms.ToTensor()
         ])
+    
+    def random_exp(self,n=1,train=True,show=False):
+        examples = []
+        for k in range(n):
+          i = random.randint(0,len(self.name_image))
+          img = self.transform(self.name_image[i])
+          #img = img/img.max()
+          #img = img**(random.random( ) *0.7 + 0.6)
+          examples.append(img)
+        if show == True:
+          fig=plt.figure(figsize=(8, 8))
+          rows = int(n/4) + 2
+          columns = int(n/8) + 2
+          for j,exp in enumerate(examples):
+            fig.add_subplot(rows, columns, j+1)
+            plt.imshow(exp.permute(1, 2, 0))
+        return examples
 
     def __getitem__(self, index):
         img = self.name_image[index]
@@ -42,4 +60,4 @@ class TextLoader(torch.utils.data.Dataset):
         return (torch.FloatTensor(img), torch.LongTensor(label))
 
     def __len__(self):
-        return len(self.label)
+        return len(self.label
