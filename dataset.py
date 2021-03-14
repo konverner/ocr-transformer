50cf02b5ce81f3076e57955d6cbc01e51c27113d
Constantin Werner <52051213+constantin50@users.noreply.github.com>
2021-03-14 20:22:15 +0700

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2, os, argparse, time, random, math
 from torchvision import transforms, models
+from collections import Counter
 import torch
 from config import *
 
@@ -29,13 +30,11 @@ class TextLoader(torch.utils.data.Dataset):
         ])
     
     def random_exp(self,n=1,train=True,show=False):
-        print('blah')
         examples = []
         for k in range(n):
-
           i = random.randint(0,len(self.name_image))
-          print(self.label[i])
           img = self.transform(self.name_image[i])
+          print(self.label[i])
           img = img/img.max()
           #img = img**(random.random( ) *0.7 + 0.6)
           examples.append(img)
@@ -47,6 +46,17 @@ class TextLoader(torch.utils.data.Dataset):
             fig.add_subplot(rows, columns, j+1)
             plt.imshow(exp.permute(1, 2, 0))
         return examples
+    
+    def get_info(self):
+        N = len(self.label)
+        max_len = -1
+        for label in self.label:
+          if len(label) > max_len:
+            max_len = len(label)
+        counter = Counter(''.join(self.label))
+        counter = dict(sorted(counter.items(), key=lambda item: item[1]))
+        print('Size of dataset: {}\nMax length of expression: {}\nThe most common char: {}\n The least common char: {}'.format(\
+        N,max_len,list(counter.items())[-1],list(counter.items())[0]))
 
     def __getitem__(self, index):
         img = self.name_image[index]
