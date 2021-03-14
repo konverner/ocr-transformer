0ae79976cef1e976c914ee1f77fa26f423ca73ba
Constantin Werner <52051213+constantin50@users.noreply.github.com>
2021-03-14 18:38:42 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,75 +1,85 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import cv2, os, argparse, time, random, math
 from torchvision import transforms, models
-from collections import Counter
 import torch
-from config import *
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
 
-# Перевести текст в массив индексов
-def text_to_labels(s, char2idx):
-    return [char2idx['SOS']] + [char2idx[i] for i in s if i in char2idx.keys()] + [char2idx['EOS']]
+class TransformerModel(nn.Module):
+    def __init__(self, name, outtoken, hidden=128, enc_layers=1, dec_layers=1, nhead=1, dropout=0.1, pretrained=False):
+        # здесь загружаем сверточную модель, например, resnet50
+        super(TransformerModel, self).__init__()
+        self.backbone = models.__getattribute__(name)(pretrained=pretrained)
+        # self.backbone.avgpool = nn.MaxPool2d((4, 1))
+        self.backbone.fc = nn.Conv2d(2048, hidden, 1)
 
-# Датасет загрузки изображений и тексты
-class TextLoader(torch.utils.data.Dataset):
-    def __init__(self ,name_image ,label, char2idx,idx2char,eval=False):
-        print('text loader:', len(name_image), len(label))
-        self.name_image = name_image
-        self.label = label
-        self.char2idx = char2idx
-        self.idx2char = idx2char
-        self.eval = eval
-        self.transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize((int(hp.height *1.05), int(hp.width *1.05))),
-            transforms.RandomCrop((hp.height, hp.width)),
-            transforms.ColorJitter(contrast=(0.5,1),saturation=(0.5,1)),
-            transforms.RandomRotation(degrees=(-2, 2)),
-            #transforms.RandomAffine(10 ,None ,[0.6 ,1] ,3 ,fillcolor=255),
-            #transforms.transforms.GaussianBlur(3, sigma=(0.1, 0.5)),
-            transforms.ToTensor()
-        ])
-    
-    def random_exp(self,n=1,train=True,show=False):
-        examples = []
-        for k in range(n):
-          i = random.randint(0,len(self.name_image))
-          img = self.transform(self.name_image[i])
-          img = img/img.max()
-          img = img**(random.random( ) *0.7 + 0.6)
-          examples.append(img)
-        if show == True:
-          fig=plt.figure(figsize=(8, 8))
-          rows = int(n/4) + 2
-          columns = int(n/8) + 2
-          for j,exp in enumerate(examples):
-            fig.add_subplot(rows, columns, j+1)
-            plt.imshow(exp.permute(1, 2, 0))
-        return examples
-    
-    def get_info(self):
-        N = len(self.label)
-        max_len = -1
-        for label in self.label:
-          if len(label) > max_len:
-            max_len = len(label)
-        counter = Counter(''.join(self.label))
-        counter = dict(sorted(counter.items(), key=lambda item: item[1]))
-        print('Size of dataset: {}\nMax length of expression: {}\nThe most common char: {}\n The least common char: {}'.format(\
-        N,max_len,list(counter.items())[-1],list(counter.items())[0]))
+        self.pos_encoder = PositionalEncoding(hidden, dropout)
+        self.decoder = nn.Embedding(outtoken, hidden)
+        self.pos_decoder = PositionalEncoding(hidden, dropout)
+        self.transformer = nn.Transformer(d_model=hidden, nhead=nhead, num_encoder_layers=enc_layers,
+                                          num_decoder_layers=dec_layers, dim_feedforward=hidden * 4, dropout=dropout,
+                                          activation='relu')
 
-    def __getitem__(self, index):
-        img = self.name_image[index]
-        if not self.eval:
-            img = self.transform(img)
-            img = img / img.max()
-            img = img**(random.random( ) *0.7 + 0.6)
-        else:
-            img = np.transpose(img ,(2 ,0 ,1))
-            img = img / img.max()
+        self.fc_out = nn.Linear(hidden, outtoken)
+        self.src_mask = None
+        self.trg_mask = None
+        self.memory_mask = None
 
-        label = text_to_labels(self.label[index], self.char2idx)
-        return (torch.FloatTensor(img), torch.LongTensor(label))
+    def generate_square_subsequent_mask(self, sz):
+        mask = torch.triu(torch.ones(sz, sz), 1)
+        mask = mask.masked_fill(mask == 1, float('-inf'))
+        return mask
 
-    def __len__(self):
-        return len(self.label)
+    def make_len_mask(self, inp):
+        return (inp == 0).transpose(0, 1)
+
+    def forward(self, src, trg):
+        if self.trg_mask is None or self.trg_mask.size(0) != len(trg):
+            self.trg_mask = self.generate_square_subsequent_mask(len(trg)).to(trg.device)
+        x = self.backbone.conv1(src)
+
+        x = self.backbone.bn1(x)
+        x = self.backbone.relu(x)
+        x = self.backbone.maxpool(x)
+        x = self.backbone.layer1(x)
+        x = self.backbone.layer2(x)
+        x = self.backbone.layer3(x)
+        x = self.backbone.layer4(x)
+        # x = self.backbone.avgpool(x)
+
+        x = self.backbone.fc(x)
+
+        x = x.permute(0, 3, 1, 2).flatten(2).permute(1, 0, 2)
+        src_pad_mask = self.make_len_mask(x[:, :, 0])
+        src = self.pos_encoder(x)
+
+        trg_pad_mask = self.make_len_mask(trg)
+        trg = self.decoder(trg)
+        trg = self.pos_decoder(trg)
+
+        output = self.transformer(src, trg, src_mask=self.src_mask, tgt_mask=self.trg_mask,
+                                  memory_mask=self.memory_mask,
+                                  src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=trg_pad_mask,
+                                  memory_key_padding_mask=src_pad_mask)
+        output = self.fc_out(output)
+
+        return output
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        self.scale = nn.Parameter(torch.ones(1))
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(
+            0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.scale * self.pe[:x.size(0), :]
+        return self.dropout(x) 
