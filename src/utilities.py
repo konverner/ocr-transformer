85d26285fe70491f58a3d176f3e7ec2cb87dbd3d
Constantin Werner <52051213+conwerner@users.noreply.github.com>
2022-05-25 23:34:07 +0700

    Update utilities.py

--- src/utilities.py
+++ src/utilities.py
@@ -11,6 +11,26 @@ from tqdm import tqdm
 from config import *
 
 
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+        self.scale = nn.Parameter(torch.ones(1))
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(
+            0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.scale * self.pe[:x.size(0), :]
+        return self.dropout(x) 
+
+    
 # convert images and labels into defined data structures
 def process_data(image_dir, labels_dir, ignore=[]):
     """
