0aa3282fe0dd181eafb538ddc1f44dede0d9ca9e
Constantin Werner <52051213+conwerner@users.noreply.github.com>
2022-05-28 00:46:52 +0700

    Update model2.py

--- src/models/model2.py
+++ src/models/model2.py
@@ -2,8 +2,9 @@ import math
 import torch
 import torch.nn as nn
 from torch.nn import Conv2d, MaxPool2d, BatchNorm2d, LeakyReLU
-from torchvision import models
 from utils import PositionalEncoding, count_parameters
+from config import DEVICE
+from const import ALPHABET
 
 class TransformerModel(nn.Module):
     def __init__(self, outtoken, hidden, enc_layers=1, dec_layers=1, nhead=1, dropout=0.1):
@@ -48,12 +49,61 @@ class TransformerModel(nn.Module):
         print(f'{count_parameters(self):,} trainable parameters')
 
     def generate_square_subsequent_mask(self, sz):
-        mask = torch.triu(torch.ones(sz, sz), 1)
+        mask = torch.triu(torch.ones(sz, sz, device=DEVICE), 1)
         mask = mask.masked_fill(mask == 1, float('-inf'))
         return mask
 
     def make_len_mask(self, inp):
         return (inp == 0).transpose(0, 1)
+    
+    def _get_features(self, src):
+        '''
+        params
+        ---
+        src : Tensor [64, 3, 64, 256] : [B,C,H,W]
+            B - batch, C - channel, H - height, W - width
+
+        returns
+        ---
+        x : Tensor : [W,B,CH]
+        '''
+        x = self.activ(self.bn0(self.conv0(src)))
+        x = self.pool1(self.activ(self.bn1(self.conv1(x))))
+        x = self.activ(self.bn2(self.conv2(x)))
+        x = self.pool3(self.activ(self.bn3(self.conv3(x))))
+        x = self.activ(self.bn4(self.conv4(x)))
+        x = self.pool5(self.activ(self.bn5(self.conv5(x))))
+        x = self.activ(self.bn6(self.conv6(x)))
+        x = x.permute(0, 3, 1, 2).flatten(2).permute(1, 0, 2)
+        return x
+
+    def predict(self, batch):
+        '''
+        params
+        ---
+        batch : Tensor [64, 3, 64, 256] : [B,C,H,W]
+            B - batch, C - channel, H - height, W - width
+        
+        returns
+        ---
+        result : List [64, -1] : [B, -1]
+            preticted sequences of tokens' indexes
+        '''
+        result = []
+        for item in batch:
+          x = self._get_features(item.unsqueeze(0))
+          memory = self.transformer.encoder(self.pos_encoder(x))
+          out_indexes = [ALPHABET.index('SOS'), ]
+          for i in range(100):
+              trg_tensor = torch.LongTensor(out_indexes).unsqueeze(1).to(DEVICE)
+              output = self.fc_out(self.transformer.decoder(self.pos_decoder(self.decoder(trg_tensor)), memory))
+
+              out_token = output.argmax(2)[-1].item()
+              out_indexes.append(out_token)
+              if out_token == ALPHABET.index('EOS'):
+                  break
+          result.append(out_indexes)
+        return result
 
     def forward(self, src, trg):
         '''
@@ -67,17 +117,7 @@ class TransformerModel(nn.Module):
         if self.trg_mask is None or self.trg_mask.size(0) != len(trg):
             self.trg_mask = self.generate_square_subsequent_mask(len(trg)).to(trg.device) 
 
-        x = self.activ(self.bn0(self.conv0(src)))
-        x = self.pool1(self.activ(self.bn1(self.conv1(x))))
-        x = self.activ(self.bn2(self.conv2(x)))
-        x = self.pool3(self.activ(self.bn3(self.conv3(x))))
-        x = self.activ(self.bn4(self.conv4(x)))
-        x = self.pool5(self.activ(self.bn5(self.conv5(x))))
-        x = self.activ(self.bn6(self.conv6(x)))
-        x = x.permute(0, 3, 1, 2)
-        x = x.flatten(2)
-        x = x.permute(1, 0, 2)
-        
+        x = self._get_features(src)
         src_pad_mask = self.make_len_mask(x[:, :, 0])
         src = self.pos_encoder(x)
         trg_pad_mask = self.make_len_mask(trg)
