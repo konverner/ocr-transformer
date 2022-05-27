0041ed32d515a45f2dbba4f4e7bf89a2ba41a82d
Constantin Werner <52051213+conwerner@users.noreply.github.com>
2022-05-28 00:46:37 +0700

    Update model1.py

--- src/models/model1.py
+++ src/models/model1.py
@@ -3,6 +3,8 @@ import torch
 import torch.nn as nn
 from torchvision import models
 from utils import PositionalEncoding, count_parameters
+from config import DEVICE
+from const import ALPHABET
 
 
 class TransformerModel(nn.Module):
@@ -30,24 +32,25 @@ class TransformerModel(nn.Module):
         print(f'{count_parameters(self):,} trainable parameters')
 
     def generate_square_subsequent_mask(self, sz):
-        mask = torch.triu(torch.ones(sz, sz), 1)
+        mask = torch.triu(torch.ones(sz, sz, device=DEVICE), 1)
         mask = mask.masked_fill(mask == 1, float('-inf'))
         return mask
 
     def make_len_mask(self, inp):
         return (inp == 0).transpose(0, 1)
-
-    def forward(self, src, trg):
+    
+    # it vectorizes images
+    def _get_features(self, src):
         '''
         params
         ---
         src : Tensor [64, 3, 64, 256] : [B,C,H,W]
             B - batch, C - channel, H - height, W - width
-        trg : Tensor [13, 64] : [L,B]
-            L - max length of label
+
+        returns
+        ---
+        x : Tensor : [W,B,CH]
         '''
-        if self.trg_mask is None or self.trg_mask.size(0) != len(trg):
-            self.trg_mask = self.generate_square_subsequent_mask(len(trg)).to(trg.device) 
         x = self.backbone.conv1(src)
 
         x = self.backbone.bn1(x)
@@ -62,9 +65,51 @@ class TransformerModel(nn.Module):
         x = x.permute(0, 3, 1, 2) # [64, 8, 256, 2] : [B,W,C,H]
         x = x.flatten(2) # [64, 8, 512] : [B,W,CH]
         x = x.permute(1, 0, 2) # [8, 64, 512] : [W,B,CH]
+        return x
+
+    def predict(self, batch):
+        '''
+        params
+        ---
+        batch : Tensor [64, 3, 64, 256] : [B,C,H,W]
+            B - batch, C - channel, H - height, W - width
         
+        returns
+        ---
+        result : List [64, -1] : [B, -1]
+            preticted sequences of tokens' indexes
+        '''
+        result = []
+        for item in batch:
+          x = self._get_features(item.unsqueeze(0))
+          memory = self.transformer.encoder(self.pos_encoder(x))
+          out_indexes = [ALPHABET.index('SOS'), ]
+          for i in range(100):
+              trg_tensor = torch.LongTensor(out_indexes).unsqueeze(1).to(DEVICE)
+              output = self.fc_out(self.transformer.decoder(self.pos_decoder(self.decoder(trg_tensor)), memory))
+
+              out_token = output.argmax(2)[-1].item()
+              out_indexes.append(out_token)
+              if out_token == ALPHABET.index('EOS'):
+                  break
+          result.append(out_indexes)
+        return result
+
+    def forward(self, src, trg):
+        '''
+        params
+        ---
+        src : Tensor [64, 3, 64, 256] : [B,C,H,W]
+            B - batch, C - channel, H - height, W - width
+        trg : Tensor [13, 64] : [L,B]
+            L - max length of label
+        '''
+        x = self._get_features(src)
         src_pad_mask = self.make_len_mask(x[:, :, 0])
         src = self.pos_encoder(x) # [8, 64, 512]
+
+        if self.trg_mask is None or self.trg_mask.size(0) != len(trg):
+          self.trg_mask = self.generate_square_subsequent_mask(len(trg)).to(trg.device)
         trg_pad_mask = self.make_len_mask(trg)
         trg = self.decoder(trg)
         trg = self.pos_decoder(trg)
@@ -73,6 +118,5 @@ class TransformerModel(nn.Module):
                                   memory_mask=self.memory_mask,
                                   src_key_padding_mask=src_pad_mask, tgt_key_padding_mask=trg_pad_mask,
                                   memory_key_padding_mask=src_pad_mask) # [13, 64, 512] : [L,B,CH]
-        output = self.fc_out(output) # [13, 64, 92] : [L,B,H]
-
-        return output
+        logits = self.fc_out(output) # [13, 64, 92] : [L,B,H]
+        return logits
