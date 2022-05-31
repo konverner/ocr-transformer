55455f39296127959d2a90c271495b2cabefdfb2
Constantin Werner <52051213+conwerner@users.noreply.github.com>
2022-05-31 16:48:15 +0700

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,4 +1,6 @@
+import random
 import torch
+import numpy as np
 from collections import Counter
 import Augmentor
 from torchvision import transforms
@@ -20,7 +22,7 @@ def text_to_labels(s, char2idx):
 
 # store list of images' names (in directory) and does some operations with images
 class TextLoader(torch.utils.data.Dataset):
-    def __init__(self, images_name, labels, transforms, char2idx, idx2char, eval=False):
+    def __init__(self, images_name, labels, transforms, char2idx, idx2char):
         """
         params
         ---
@@ -35,7 +37,6 @@ class TextLoader(torch.utils.data.Dataset):
         self.labels = labels
         self.char2idx = char2idx
         self.idx2char = idx2char
-        self.eval = eval
         self.transform = transforms
 
     def _transform(self, X):
@@ -46,47 +47,7 @@ class TextLoader(torch.utils.data.Dataset):
             return tt(ld(vignet(X)))
         if j == 2:
             return tt(ld(un(X)))
-
-    # returns (and shows) random data examples
-    def random_exp(self, n=1, show=False, fix=False):
-        """
-        params
-        ---
-        n : int
-            number of examples to return (to show)
-        show : bool
-            show or not
-        fix : bool
-            if it is true, function will return the same images again and again but with different augmentations
-
-        returns
-        ---
-        examples : list of PIL.Image objects
-        """
-        examples = []
-        if fix == True:
-            for i in range(n):
-                img = self._transform(self.images_name[i])
-                print(self.label[i])
-                img = img / img.max()
-                img = img ** (random.random() * 0.7 + 0.6)
-                examples.append(img)
-        else:
-            for k in range(n):
-                i = random.randint(0, len(self.images_name))
-                img = self._transform(self.images_name[i])
-                print(self.label[i])
-                img = img / img.max()
-                img = img ** (random.random() * 0.7 + 0.6)
-                examples.append(img)
-        if show == True:
-            fig = plt.figure(figsize=(8, 8))
-            rows = int(n / 4) + 2
-            columns = int(n / 8) + 2
-            for j, exp in enumerate(examples):
-                fig.add_subplot(rows, columns, j + 1)
-                plt.imshow(exp.permute(1, 2, 0))
-        return examples
+            
 
     # shows some stats about dataset
     def get_info(self):
@@ -103,13 +64,9 @@ class TextLoader(torch.utils.data.Dataset):
 
     def __getitem__(self, index):
         img = self.images_name[index]
-        if not self.eval:
-            img = self.transform(img)
-            img = img / img.max()
-            img = img ** (random.random() * 0.7 + 0.6)
-        else:
-            img = np.transpose(img, (2, 0, 1))
-            img = img / img.max()
+        img = self.transform(img)
+        img = img / img.max()
+        img = img ** (random.random() * 0.7 + 0.6)
 
         label = text_to_labels(self.labels[index], self.char2idx)
         return (torch.FloatTensor(img), torch.LongTensor(label))
