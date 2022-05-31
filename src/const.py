8af3f18b2d6fbb9ab41f41675f2af60f84ce42c4
Constantin Werner <52051213+conwerner@users.noreply.github.com>
2022-05-31 13:41:55 +0700

    Update const.py

--- src/const.py
+++ src/const.py
@@ -1,19 +1,47 @@
-DIR = '/content/' # work directory
-PATH_TEST_DIR = DIR+'test/'
-PATH_TEST_LABELS =  DIR+'test.tsv'
-PATH_TRAIN_DIR =  DIR+'train/'
-PATH_TRAIN_LABELS =  DIR+'train.tsv'
-PREDICT_PATH = "/content/test/"
-CHECKPOINT_PATH = DIR
-WEIGHTS_PATH = "/content/checkpoint_9.pt"
-PATH_TEST_RESULTS = DIR+'/test_result.tsv'
-TRAIN_LOG = DIR+'/train_log.tsv'
+import torch
+from torchvision import transforms
+import Augmentor
+import random
 
-ALPHABET = ['PAD', 'SOS', ' ', '!', '"', '%', '(', ')', ',', '-', '.', '/',\
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?',\
-            '[', ']', '«', '»', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И',\
-            'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х',\
-            'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е',\
-            'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',\
-            'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',\
-            'ё', 'EOS']
+
+### MODEL ### 
+
+MODEL = 'model1'
+HIDDEN = 512
+ENC_LAYERS = 2
+DEC_LAYERS = 2
+N_HEADS = 4
+LENGTH = 32
+
+### TRAINING ###
+
+LR = 1e-4
+BATCH_SIZE = 32
+DROPOUT = 0.1
+N_EPOCHS = 128
+CHECKPOINT_FREQ = 10 # save checkpoint every 10 epochs
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+RANDOM_SEED = 42
+
+# IMAGE SIZE
+WIDTH = 256
+HEIGHT = 64
+CHANNELS = 3
+
+
+### AUGMENTATIONS ###
+
+p = Augmentor.Pipeline()
+p.shear(max_shear_left=2, max_shear_right=2, probability=0.7)
+p.random_distortion(probability=1.0, grid_width=3, grid_height=3, magnitude=11)
+
+TRANSFORMS = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Grayscale(CHANNELS),
+            p.torch_transform(),  # random distortion and shear
+            transforms.ColorJitter(contrast=(0.5,1),saturation=(0.5,1)),
+            transforms.RandomRotation(degrees=(-9, 9), fill=255),
+            transforms.RandomAffine(10, None, [0.6 ,1] ,3 ,fillcolor=255),
+            transforms.transforms.GaussianBlur(3, sigma=(0.1, 1.9)),
+            transforms.ToTensor()
+        ])
