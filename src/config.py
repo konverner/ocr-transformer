c5e9beec5b8b18897a8a6a0f638d7eeac5ba747e
Constantin Werner <52051213+conwerner@users.noreply.github.com>
2022-06-08 01:17:55 +0700

    Update config.py

--- src/config.py
+++ src/config.py
@@ -5,43 +5,44 @@ import random
 
 
 ### MODEL ### 
-
 MODEL = 'model1'
 HIDDEN = 512
 ENC_LAYERS = 2
 DEC_LAYERS = 2
 N_HEADS = 4
 LENGTH = 32
-ALPHABET = ['PAD', 'SOS', ' ', '!', '"', '%', '(', ')', ',', '-', '.', '/',\
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?',\
-            '[', ']', '«', '»', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И',\
-            'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х',\
-            'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е',\
-            'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',\
-            'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',\
+ALPHABET = ['PAD', 'SOS', ' ', '!', '"', '%', '(', ')', ',', '-', '.', '/',
+            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '?',
+            '[', ']', '«', '»', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И',
+            'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х',
+            'Ц', 'Ч', 'Ш', 'Щ', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е',
+            'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т',
+            'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
             'ё', 'EOS']
             
 ### TRAINING ###
-
-BATCH_SIZE = 32
-DROPOUT = 0.1
+BATCH_SIZE = 16
+DROPOUT = 0.5
 N_EPOCHS = 128
 CHECKPOINT_FREQ = 10 # save checkpoint every 10 epochs
-DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+DEVICE = 'cuda:1' #torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 RANDOM_SEED = 42
 SCHUDULER_ON = True # "ReduceLROnPlateau"
 PATIENCE = 5 # for ReduceLROnPlateau
 OPTIMIZER_NAME = 'Adam' # or "SGD"
-LR = 4e-4
+LR = 2e-6
 
-# IMAGE SIZE
+### TESTING ###
+CASE = True # is case taken into account or not while evaluating
+PUNCT = False # are punctuation marks taken into account
+
+### INPUT IMAGE PARAMETERS ###
 WIDTH = 256
 HEIGHT = 64
 CHANNELS = 3 # 3 channels if model1
 
 
 ### AUGMENTATIONS ###
-
 p = Augmentor.Pipeline()
 p.shear(max_shear_left=2, max_shear_right=2, probability=0.7)
 p.random_distortion(probability=1.0, grid_width=3, grid_height=3, magnitude=11)
@@ -51,9 +52,9 @@ TRAIN_TRANSFORMS = transforms.Compose([
             transforms.Grayscale(CHANNELS),
             p.torch_transform(),  # random distortion and shear
             transforms.ColorJitter(contrast=(0.5,1),saturation=(0.5,1)),
-            transforms.RandomRotation(degrees=(-9, 9), fill=255),
+            transforms.RandomRotation(degrees=(-9, 9)),
             transforms.RandomAffine(10, None, [0.6 ,1] ,3 ,fillcolor=255),
-            transforms.transforms.GaussianBlur(3, sigma=(0.1, 1.9)),
+            #transforms.transforms.GaussianBlur(3, sigma=(0.1, 1.9)),
             transforms.ToTensor()
         ])
 
